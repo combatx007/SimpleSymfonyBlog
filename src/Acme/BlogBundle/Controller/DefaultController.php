f8ff358bf4f7bd3dbb697343d445787f5caa74c1
mrseo <mr.seo@mail.ru>
2013-04-26 13:29:37 +0300

    поправил недочеты навигации, ошибку с отображением главной (были неопределенные переменные в шаблоне)

--- src/Acme/BlogBundle/Controller/DefaultController.php
+++ src/Acme/BlogBundle/Controller/DefaultController.php
@@ -10,9 +10,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getEntityManager();
+        $query = $em->createQuery('SELECT COUNT(u.id) FROM Acme\BlogBundle\Entity\Post u');
+        $count = $query->getSingleScalarResult();
+        $limit = 3;
+        $count_pages = ceil($count / $limit);
         $posts = $this->getDoctrine()->getRepository('AcmeBlogBundle:Post');
         $posts = $posts->findBy(array(),array('updated'=>'DESC'),3,0);
-        return $this->render('AcmeBlogBundle:Default:index.html.twig',array('posts' => $posts));
+        return $this->render('AcmeBlogBundle:Default:index.html.twig',array('posts' => $posts, 'id' => '1', 'pages' => '$count_pages'));
     }
 
     public function pageAction($id)
@@ -24,7 +29,7 @@ class DefaultController extends Controller
         $count_pages = ceil($count / $limit);
         $offset = ($id - 1) * $limit;
         $posts = $em->getRepository('AcmeBlogBundle:Post')->findBy(array(), array('updated'=>'DESC'), $limit, $offset);
-        return $this->render('AcmeBlogBundle:Default:index.html.twig', array('posts' => $posts,'pages' => $count_pages,
+        return $this->render('AcmeBlogBundle:Default:page.html.twig', array('posts' => $posts,'pages' => $count_pages,
         'id' => $id));
     }
 }
